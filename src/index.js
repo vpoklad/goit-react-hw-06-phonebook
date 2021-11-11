7c3aa32814b1bc2a4e8737ddc417c703334116cb
Viktor Poklad <3d.poklad@gmail.com>
2021-11-11 17:11:18 +0200

    добавляет localStorage

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import { Provider } from 'react-redux';
 import './index.css';
 import 'modern-normalize/modern-normalize.css';
 import App from './App';
-import { store } from './redux/store';
+import { store, persistor } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={<p>Loading...</p>} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,40 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+
 // import { createStore } from 'redux';
 // import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './reducer';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
-export const store = configureStore({ reducer: rootReducer });
+const persistConfig = {
+  key: 'сontacts',
+  storage,
+  blacklist: ['filter'],
+};
+
+// const middleware = [ ...getDefaultMiddleware({serializableCheck: {
+//     ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+//   }})
+
+// ]
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+});
+export const persistor = persistStore(store);
 
 // export const store = createStore(rootReducer, composeWithDevTools());
