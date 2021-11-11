13933a13de5bb8c0b1de99cd32902be2ba14c898
Viktor Poklad <3d.poklad@gmail.com>
2021-11-11 11:23:38 +0200

    рефоактор на toolkit

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,12 +1,17 @@
-import { types } from '../redux/types';
+import { createAction } from '@reduxjs/toolkit';
+// import { types } from '../redux/types';
 
-export function addContact(payload) {
-  return { type: types.ADD_CONTACT, payload };
-}
-export function deleteContact(payload) {
-  return { type: types.DELETE_CONTACT, payload };
-}
+export const addContact = createAction('contacts/add');
+export const deleteContact = createAction('contacts/del');
+export const setFilter = createAction('filter/set');
 
-export function setFilter(payload) {
-  return { type: types.SET_FILTER, payload };
-}
+// export function addContact(payload) {
+//   return { type: types.ADD_CONTACT, payload };
+// }
+// export function deleteContact(payload) {
+//   return { type: types.DELETE_CONTACT, payload };
+// }
+
+// export function setFilter(payload) {
+//   return { type: types.SET_FILTER, payload };
+// }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,46 +1,8 @@
-import { combineReducers, createStore } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import { types } from '../redux/types';
+import { configureStore } from '@reduxjs/toolkit';
+// import { createStore } from 'redux';
+// import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './reducer';
 
-const initialContacts = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-];
+export const store = configureStore({ reducer: rootReducer });
 
-const contactReducer = (state = initialContacts, { type, payload }) => {
-  switch (type) {
-    case types.ADD_CONTACT:
-      const existContact = state.some(
-        el => el.name.toLowerCase() === payload.name.toLowerCase(),
-      );
-      if (existContact) {
-        alert(`this contact already exists`);
-        return state;
-      } else {
-        return [...state, payload];
-      }
-    case types.DELETE_CONTACT:
-      return state.filter(contact => contact.id !== payload);
-    default:
-      return state;
-  }
-};
-
-const filterReducer = (state = '', { type, payload }) => {
-  switch (type) {
-    case types.SET_FILTER:
-      return payload;
-
-    default:
-      return state;
-  }
-};
-
-const rootReducer = combineReducers({
-  contacts: contactReducer,
-  filter: filterReducer,
-});
-
-export const store = createStore(rootReducer, composeWithDevTools());
+// export const store = createStore(rootReducer, composeWithDevTools());
